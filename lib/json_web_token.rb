2e34972691a732f540fa5d425ce4146583808b2f
venu bhat <venubha147@gmail.com>
2025-03-28 20:38:07 +0530

    Added secret_key_base for production

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,6 +1,6 @@
 # app/lib/json_web_token.rb
 class JsonWebToken
-  SECRET_KEY = Rails.application.credentials.secret_key_base
+  SECRET_KEY = "2c6a000c287b677ea5d15d696109d582d461c5a70a2cdee29e717bf5f4446eb10fb7cb2bc9378f6688de67c678699512df4fd074f469acc01236c5d03e768656"
 
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
