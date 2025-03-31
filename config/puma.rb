1c0d4628514d652e9259912944f2e885f7842df5
venu bhat <venubha147@gmail.com>
2025-03-31 17:03:53 +0530

    test

--- config/puma.rb
+++ config/puma.rb
@@ -1,50 +1,41 @@
-# Puma configuration for AWS Elastic Beanstalk
-workers Integer(ENV.fetch("WEB_CONCURRENCY", 2))
-threads_count = Integer(ENV.fetch("RAILS_MAX_THREADS", 5))
+# This configuration file will be evaluated by Puma. The top-level methods that
+# are invoked here are part of Puma's configuration DSL. For more information
+# about methods provided by the DSL, see https://puma.io/puma/Puma/DSL.html.
+#
+# Puma starts a configurable number of processes (workers) and each process
+# serves each request in a thread from an internal thread pool.
+#
+# You can control the number of workers using ENV["WEB_CONCURRENCY"]. You
+# should only set this value when you want to run 2 or more workers. The
+# default is already 1.
+#
+# The ideal number of threads per worker depends both on how much time the
+# application spends waiting for IO operations and on how much you wish to
+# prioritize throughput over latency.
+#
+# As a rule of thumb, increasing the number of threads will increase how much
+# traffic a given process can handle (throughput), but due to CRuby's
+# Global VM Lock (GVL) it has diminishing returns and will degrade the
+# response time (latency) of the application.
+#
+# The default is set to 3 threads as it's deemed a decent compromise between
+# throughput and latency for the average Rails application.
+#
+# Any libraries that use a connection pool or another resource pool should
+# be configured to provide at least as many connections as the number of
+# threads. This includes Active Record's `pool` parameter in `database.yml`.
+threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
-# Use the port provided by Elastic Beanstalk
+# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT", 3000)
 
-# Environment settings
-environment ENV.fetch("RAILS_ENV", "production")
-
-# Directory for deployment
-directory "/var/app/current"
-
-# Allow puma to be restarted by `bin/rails restart` command
+# Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
 
-# Run the Solid Queue supervisor inside of Puma if enabled
+# Run the Solid Queue supervisor inside of Puma for single-server deployments
 plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
 
-# PID file location for EB
-pidfile "/var/run/puma.pid"
-
-# Logging configuration
-stdout_redirect "/var/log/puma.stdout.log", "/var/log/puma.stderr.log", true
-
-# Allow EB to properly manage the process
-bind "unix:///var/run/puma.sock"
-daemonize false
-
-# Worker timeout (important for EB health checks)
-worker_timeout 30
-
-# Preload the application for faster worker boot
-preload_app!
-
-# Connection handling for database/redis
-on_worker_boot do
-  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
-  # Reconnect to Redis if needed
-  if defined?(Sidekiq)
-    Sidekiq.configure_client do |config|
-      config.redis = { url: ENV['REDIS_URL'], size: Integer(ENV.fetch("SIDEKIQ_REDIS_POOL_SIZE", 5)) }
-    end
-  end
-end
-
-before_fork do
-  ActiveRecord::Base.connection_pool.disconnect! if defined?(ActiveRecord)
-end
+# Specify the PID file. Defaults to tmp/pids/server.pid in development.
+# In other environments, only set the PID file if requested.
+pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
