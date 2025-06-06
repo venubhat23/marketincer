9ab03c81a15bd22b3f8197263a9989739c4a47ac
venu bhat <venubha147@gmail.com>
2025-06-06 19:04:04 +0530

    tst

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :social_page, optional: true
 
-  validates :account_id, presence: true
+  # validates :account_id, presence: true
 
   validates :s3_url, presence: true
   validates :status, presence: true
