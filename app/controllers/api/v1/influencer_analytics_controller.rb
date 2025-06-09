71cde81b055f18a44d5f37f0644d8b1e5aeac7a2
venu bhat <venubha147@gmail.com>
2025-06-09 23:09:27 +0530

    Test

--- app/controllers/api/v1/influencer_analytics_controller.rb
+++ app/controllers/api/v1/influencer_analytics_controller.rb
@@ -361,7 +361,8 @@ module Api
             likes: post_item[:like_count] || 0,
             comments: post_item[:comments_count] || 0,
             shares: post_insights['shares'] || 0,
-            thumbnail_url: post_item[:media_url] || "https://via.placeholder.com/300x300"
+            thumbnail_url: post_item[:media_url] || "https://via.placeholder.com/300x300",
+            image_url: post.picture_url
           }
         end
 
