8c77883c2c3fd0043fd0648dbb384fc2e28f3523
venu bhat <venubha147@gmail.com>
2025-06-14 23:22:52 +0530

    test

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,12 @@
 # app/mailers/user_mailer.rb
 class UserMailer < ApplicationMailer
+
   def activation_email(user)
     @user = user
-    # Update the activation URL to include the API namespace
     @activation_url = "https://api.marketincer.com/api/v1/activate/#{user.activation_token}"
-    
-    mail(
-      to: @user.email,
-      subject: 'Activate your account'
-    )
+    mail(to: @user.email, subject: 'Activate your account')
   end
 end
+
+
+
